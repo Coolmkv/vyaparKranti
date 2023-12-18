7b4295372a22402b811356dd5f14bbcd27ade8dc
vyaparkranti <vyaparkranti@gmail.com>
2023-12-18 15:48:52 +0530

    AdSense on head

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -32,6 +32,9 @@
     <!-- Link Swiper's CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4915291902491386"
+     crossorigin="anonymous"></script>
+
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
